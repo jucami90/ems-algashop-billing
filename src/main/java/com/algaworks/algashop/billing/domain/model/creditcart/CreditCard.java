36718f83c74d96a09e64dd8dc639de1e0f3c5f90
jucami <jucami90@gmail.com>
2026-05-27 02:33:25 -0300

    update inprovement on entities

--- src/main/java/com/algaworks/algashop/billing/domain/model/creditcart/CreditCard.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcart/CreditCard.java
@@ -1,14 +1,18 @@
 package com.algaworks.algashop.billing.domain.model.creditcart;
 
+import com.algaworks.algashop.billing.domain.model.IdGenerator;
 import lombok.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Setter(AccessLevel.PRIVATE)
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreditCard {
     @EqualsAndHashCode.Include
     private UUID id;
@@ -18,7 +22,38 @@ public class CreditCard {
     private String brand;
     private Integer expMonth;
     private Integer expYear;
-
-    @Setter(AccessLevel.PUBLIC)
     private String gatewayCode;
+
+    public static CreditCard brandNew(UUID customerId,
+                                      String lastNumbers,
+                                      String brand,
+                                      Integer expMonth,
+                                      Integer expYear,
+                                      String gatewayCreditCardCode) {
+        Objects.requireNonNull(customerId);
+        Objects.requireNonNull(expMonth);
+        Objects.requireNonNull(expYear);
+
+        if (StringUtils.isAnyBlank(lastNumbers, brand, gatewayCreditCardCode)) {
+            throw new IllegalArgumentException();
+        }
+
+        return new CreditCard(
+                IdGenerator.generateTimeBasedUUID(),
+                OffsetDateTime.now(),
+                customerId,
+                lastNumbers,
+                brand,
+                expMonth,
+                expYear,
+                gatewayCreditCardCode
+        );
+    }
+
+    public void setGatewayCode(String gatewayCode) {
+        if (StringUtils.isBlank(gatewayCode)) {
+            throw new IllegalArgumentException();
+        }
+        this.gatewayCode = gatewayCode;
+    }
 }
--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/PaymentSettings.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/PaymentSettings.java
@@ -1,13 +1,18 @@
 package com.algaworks.algashop.billing.domain.model.invoice;
 
+import com.algaworks.algashop.billing.domain.model.DomainException;
+import com.algaworks.algashop.billing.domain.model.IdGenerator;
+import io.micrometer.common.util.StringUtils;
 import lombok.*;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Setter(AccessLevel.PRIVATE)
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class PaymentSettings {
 
     @EqualsAndHashCode.Include
@@ -15,4 +20,28 @@ public class PaymentSettings {
     private UUID creditCardId;
     private String gatewayCode;
     private PaymentMethod method;
+
+    static PaymentSettings brandNew(PaymentMethod method, UUID creditCardId) {
+        Objects.requireNonNull(method);
+        if (method.equals(PaymentMethod.CREDIT_CARD)) {
+            Objects.requireNonNull(creditCardId);
+        }
+        return new PaymentSettings(
+                IdGenerator.generateTimeBasedUUID(),
+                creditCardId,
+                null,
+                method
+        );
+    }
+
+    void assignGatewayCode(String gatewayCode) {
+        if (StringUtils.isBlank(gatewayCode)) {
+            throw new IllegalArgumentException();
+        }
+        if (this.getGatewayCode() != null) {
+            throw new DomainException("Gateway code already assigned");
+        }
+        setGatewayCode(gatewayCode);
+    }
+
 }
