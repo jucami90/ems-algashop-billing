2b11cc5d3eeac1948dd1f7d05b3bab365c2c41a9
jucami <jucami90@gmail.com>
2026-05-27 02:08:47 -0300

    add model

---
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcart/CreditCard.java
@@ -0,0 +1,19 @@
+package com.algaworks.algashop.billing.domain.model.creditcart;
+
+import lombok.EqualsAndHashCode;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CreditCard {
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private OffsetDateTime createdAt;
+    private UUID customerId;
+    private String lastNumbers;
+    private String brand;
+    private Integer expMonth;
+    private Integer expYear;
+    private String gatewayCode;
+}
